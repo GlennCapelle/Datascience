5a6ed53f75f7d33b29837e8e00f2b00c2aacfac6
GlennCapelle <61188116+GlennCapelle@users.noreply.github.com>
2021-02-09 16:49:54 +0100

    Update dbh.inc.php

--- website/include/dbh.inc.php
+++ website/include/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$dBUsername = "glenn";
-$dBPassword = "Glenn1234!";
-$dBName = "DataScience";
+$dBUsername = "*****";
+$dBPassword = "*****";
+$dBName = "*****";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
